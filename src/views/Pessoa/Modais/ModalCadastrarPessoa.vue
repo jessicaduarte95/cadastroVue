9dd4df53923a6c160a39089a9880de67146aa563
jessicaduarte95 <jessicaduarte.95@gmail.com>
2024-05-25 16:08:12 -0300

    fix: tratamento de erros na modal de cadastrar pessoa

--- src/views/Pessoa/Modais/ModalCadastrarPessoa.vue
+++ src/views/Pessoa/Modais/ModalCadastrarPessoa.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { PropType } from 'vue';
+import { PropType, onUpdated } from 'vue';
 import ModalComponent from '../../../components/ModalComponent.vue';
 import ButtonComponent from '../../../components/ButtonComponent.vue';
 import axios from 'axios';
@@ -46,6 +46,15 @@ export default {
 
 		const formV$ = useVuelidate(rules, form);
 
+		const errorNome = ref(false);
+		const errorEmail = ref(false);
+
+		const closeModalCadastro = () => {
+			errorNome.value = false;
+			errorEmail.value = false;
+            props.closeModalCadastro();
+        };
+
 		const submitFormRegister = async (event: Event) => {
 			event.preventDefault();
 			await formV$.value.$touch();
@@ -66,16 +75,28 @@ export default {
 					.finally(() => {
 						form.value.nome = '';
 						form.value.email = '';
-						props.closeModalCadastro();
+						closeModalCadastro();
 					});
 			} else {
-				// alert('Campos Obrigatórios')
+				if (formV$.value.$errors[0].$property == 'nome') {
+					errorNome.value = true;
+				}
+				if (formV$.value.$errors[0].$property == 'email' || formV$.value.$errors[1].$property == 'email') {
+					errorEmail.value = true;
+				}
 			}
 		};
+
+		onUpdated(() => {
+			errorNome;
+		});
+
 		return {
 			formV$,
 			form,
-			submitFormRegister
+			submitFormRegister,
+			errorNome,
+			errorEmail
 		};
 	}
 };
@@ -88,16 +109,18 @@ export default {
 				<div class="modal-container">
 					<div>
 						<h2>Cadastrar Pessoa</h2>
-						<button class="close-button" @click="$emit('close-modal')">x</button>
+						<button class="close-button" @click="closeModalCadastro()">x</button>
 					</div>
 					<div class="containerInputRegister">
 						<div class="inputFields">
 							<label class="label">Nome</label>
 							<input class="input" placeholder="Digite aqui" v-model="form.nome" type="text" />
+							<span v-if="errorNome && form.nome.length == 0" class="error">Campo obrigatório!</span>
 						</div>
 						<div class="inputFields">
 							<label class="label">Email</label>
 							<input class="input" placeholder="Digite aqui" v-model="form.email" type="email" />
+							<span v-if="errorEmail && form.email.length == 0" class="error">Campo obrigatório!</span>
 						</div>
 					</div>
 					<div class="button">
@@ -122,6 +145,14 @@ export default {
 	flex-direction: column;
 	width: 100%;
 	gap: 0.8rem;
+}
+
+.error {
+	display: flex;
+	color: red;
+	font-size: 0.8rem;
+	font-weight: 200;
+	margin-top: 0.4rem;
 }
 
 .button {
